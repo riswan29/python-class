325e08d4ecf9ff915df052d1ae39881214d76d7c
riswan29 <wandra290401@gmail.com>
2023-02-02 21:22:56 +0700

    looping for

--- pertemuan5/lat1.py
+++ pertemuan5/lat1.py
@@ -3,18 +3,19 @@
 tinggi_mhs = input("Input tinggi tiap mahasiswa : ").split(",")
 # print(tinggi_mhs)
 
-for mhs in range(0,len(tinggi_mhs)):
-    tinggi_mhs[mhs] = int(tinggi_mhs[mhs])
-# print(tinggi_mhs)
+for i in range(len(tinggi_mhs)):
+    tinggi_mhs[i] = int(tinggi_mhs[i])
+print(tinggi_mhs)
 
 panjang = len(tinggi_mhs)
 total = sum(tinggi_mhs)
 rata_rata = total / panjang
-print(round(rata_rata))
+rt=(round(rata_rata))
+print(f"rata rata = {rt}")
 
-# nentuin panjang alt_len
+# # nentuin panjang alt_len
 
 nomor_mhs = 0
 for mhs in tinggi_mhs:
     nomor_mhs = nomor_mhs + 1
-print(nomor_mhs)
+print(f"nomor mhs = {nomor_mhs}")
--- pertemuan5/lat2.py
+++ pertemuan5/lat2.py
@@ -1,14 +1,13 @@
 # tampilkan nilai tertinggi
 n_tinggi = input("Input masukkan nilai tertinggi : ").split()
-# print(n_tinggi)
+print(n_tinggi)
 
 for nilai in range(0,len(n_tinggi)):
     n_tinggi[nilai] = int(n_tinggi[nilai])
 print(n_tinggi)
 
-nilai_tinggi = 0
-for nilai in n_tinggi:
-    if nilai > nilai_tinggi:
-        nilai_tinggi = nilai
-print(nilai_tinggi)
-
+# nilai_tinggi = 0
+# for nilai in n_tinggi:
+#     if nilai > nilai_tinggi:
+#         nilai_tinggi = nilai
+# print(nilai_tinggi)
